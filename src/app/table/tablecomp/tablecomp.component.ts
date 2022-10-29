2978c1e69cfaaed7eb7f14c9c48a50bb25ac7009
Mohammed Azhar <mohammedazhar@GMG.LOCAL>
2022-10-30 01:11:25 +0530

    second commit

--- src/app/table/tablecomp/tablecomp.component.ts
+++ src/app/table/tablecomp/tablecomp.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Renderer2, OnInit, ElementRef, ViewChild, AfterViewInit, VERSION } from '@angular/core';
 
 @Component({
   selector: 'app-tablecomp',
@@ -7,90 +7,59 @@ import { Component, OnInit } from '@angular/core';
 })
 
 
-export class TablecompComponent implements OnInit {
+export class TablecompComponent {
 
   constructor() { }
-  ngOnInit(): void {
-  }
+  // ngOnInit(): void {
+  // }
 
   // color="yellow";
 
-   employeeDatas=[
-                {employeeId:"1",employeeName:"Athila Rahim",employeeAge:"23"},
-                {employeeId:"2",employeeName:"Akshay Poosan",employeeAge:"26"},
-                {employeeId:"3",employeeName:"Hanan Binth Basheer",employeeAge:"24"},
-                {employeeId:"4",employeeName:"Aysha Parveen",employeeAge:"23"},
-                {employeeId:"5",employeeName:"Sreeram",employeeAge:"25"},
-                {employeeId:"6",employeeName:"Darshan",employeeAge:"23"},
-                {employeeId:"7",employeeName:"Afsal Rahman",employeeAge:"25"},
-                {employeeId:"8",employeeName:"Fawaz MK",employeeAge:"22"},
-                {employeeId:"9",employeeName:"Sharron",employeeAge:"22"},
-                {employeeId:"10",employeeName:"Razal",employeeAge:"23"},
+  employeeDatas = [
+    { employeeId: "1", employeeName: "Athila Rahim", employeeAge: "23" ,visible:false},
+    { employeeId: "2", employeeName: "Akshay Poosan", employeeAge: "26",visible:false },
+    { employeeId: "3", employeeName: "Hanan Binth Basheer", employeeAge: "24",visible:false },
+    { employeeId: "4", employeeName: "Aysha Parveen", employeeAge: "23",visible:false },
+    { employeeId: "5", employeeName: "Sreeram", employeeAge: "25",visible:false },
+    { employeeId: "6", employeeName: "Darshan", employeeAge: "23",visible:false },
+    { employeeId: "7", employeeName: "Afsal Rahman", employeeAge: "25",visible:false },
+    { employeeId: "8", employeeName: "Fawaz MK", employeeAge: "22",visible:false },
+    { employeeId: "9", employeeName: "Sharron", employeeAge: "22" ,visible:false},
+    { employeeId: "10", employeeName: "Razal", employeeAge: "23",visible:false },
 
-              ]
+  ]
 
-
-  hello(value:string,emp:string){
+  changeEmpId(val:any,i:any){
+    this.employeeDatas[i].employeeId=val;
+    this.employeeDatas[i].visible=false
     
-    
-    
-    
-    var g = document.getElementById(value) as HTMLInputElement
-    
-    var Array:any=[]
-    Array=this.employeeDatas
-
-    var curr ="";
-    curr=`${g.id}`
-    // console.log(f);
-    
-    g.innerHTML=`<td id=${curr}><input type="text" id="input" name="fname"> <button id ="submit" class="button">submit</button></td>`
-
-    let backbutton = document.getElementById('submit')
-    backbutton?.addEventListener('click', function handleClick(event) {
-      var value = (document.getElementById("input") as HTMLInputElement).value as string
-
-      display(curr,value,emp,Array)
-
-    })
-    
-
   }
+  changeEmpName(val:any,i:any){
+    this.employeeDatas[i].employeeName=val;
+    this.employeeDatas[i].visible=false
+    
+  }
+  changeEmpAge(val:any,i:any){
+    this.employeeDatas[i].employeeAge=val;
+    this.employeeDatas[i].visible=false
+    
+    
+  }
+  
+
 
 }
 
-function display(curr: string, value: string,emp:string,empDatas:any) {
 
 
-  for (var i=0;i<empDatas.length;i++){
-    if(emp=='employeeId'){
 
-      if (empDatas[i].employeeId==curr){
-          empDatas[i].employeeId=value
-      }
 
-    }
 
-  }
 
-  var disp = document.querySelector(".tableBody") as HTMLInputElement
-  disp.innerHTML="";
 
-  for(var j=0;j<empDatas.length;j++){
-    disp.innerHTML+=` <tr >
-    <td appHighlight (dblclick)="hello(emp.employeeId,'employeeId')" id ={{emp.employeeId}} >{{emp.employeeId}}</td>
-    <td appHighlight  id ={{emp.employeeId}}>{{emp.employeeName}}</td>
-    <td appHighlight  id ={{emp.employeeId}}>{{emp.employeeAge}}</td>
-</tr>`
 
-  }
-  
-  
-  
-  
-  
 
-  
-  
-}
+
+
+
 
