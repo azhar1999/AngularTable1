0462d634aa10f5745ccdcdac7ca7d9f7b805f9cd
Mohammed Azhar <mohammedazhar@GMG.LOCAL>
2022-10-31 23:57:29 +0530

    lastCommit

--- src/app/table/tablecomp/tablecomp.component.ts
+++ src/app/table/tablecomp/tablecomp.component.ts
@@ -13,34 +13,39 @@ export class TablecompComponent {
   // ngOnInit(): void {
   // }
 
+   togglevariable="";
+
   employeeDatas = [
-    { employeeId: "1", employeeName: "Athila Rahim", employeeAge: "23" ,visible:""},
-    { employeeId: "2", employeeName: "Akshay Poosan", employeeAge: "26",visible:"" },
-    { employeeId: "3", employeeName: "Hanan Binth Basheer", employeeAge: "24",visible:"" },
-    { employeeId: "4", employeeName: "Aysha Parveen", employeeAge: "23",visible:"" },
-    { employeeId: "5", employeeName: "Sreeram", employeeAge: "25",visible:"" },
-    { employeeId: "6", employeeName: "Darshan", employeeAge: "23",visible:"" },
-    { employeeId: "7", employeeName: "Afsal Rahman", employeeAge: "25",visible:"" },
-    { employeeId: "8", employeeName: "Fawaz MK", employeeAge: "22",visible:"" },
-    { employeeId: "9", employeeName: "Sharron", employeeAge: "22" ,visible:""},
-    { employeeId: "10", employeeName: "Razal", employeeAge: "23",visible:"" },
+    { employeeId: "1", employeeName: "Athila Rahim", employeeAge: "23" },
+    { employeeId: "2", employeeName: "Akshay Poosan", employeeAge: "26" },
+    { employeeId: "3", employeeName: "Hanan Binth Basheer", employeeAge: "24" },
+    { employeeId: "4", employeeName: "Aysha Parveen", employeeAge: "23"},
+    { employeeId: "5", employeeName: "Sreeram", employeeAge: "25" },
+    { employeeId: "6", employeeName: "Darshan", employeeAge: "23"},
+    { employeeId: "7", employeeName: "Afsal Rahman", employeeAge: "25"},
+    { employeeId: "8", employeeName: "Fawaz MK", employeeAge: "22"},
+    { employeeId: "9", employeeName: "Sharron", employeeAge: "22" },
+    { employeeId: "10", employeeName: "Razal", employeeAge: "23"},
 
   ]
 
+  toggle(val:any,input:any){
+    this.togglevariable=val+input
+  }
+
+
   changeEmpId(val1:any,i:any){
     this.employeeDatas[i].employeeId=val1;
-
-    this.employeeDatas[i].visible=''  
+    this.togglevariable=''
   }
   changeEmpName(val1:any,i:any){
 
     this.employeeDatas[i].employeeName=val1;
-
-    this.employeeDatas[i].visible=''  
+    this.togglevariable=''
   }
   changeEmpAge(val1:any,i:any){
     this.employeeDatas[i].employeeAge=val1;
-    this.employeeDatas[i].visible=''  
+    this.togglevariable='' 
   }
 }
 
