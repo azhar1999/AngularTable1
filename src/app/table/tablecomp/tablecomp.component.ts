377e52606a169b5f0593f1d7b95e5e034c959800
Mohammed Azhar <mohammedazhar@GMG.LOCAL>
2022-10-31 13:04:04 +0530

    finallCOMMIT

--- src/app/table/tablecomp/tablecomp.component.ts
+++ src/app/table/tablecomp/tablecomp.component.ts
@@ -14,24 +14,34 @@ export class TablecompComponent {
   // }
 
   employeeDatas = [
-    { employeeId: "1", employeeName: "Athila Rahim", employeeAge: "23" ,visible:false},
-    { employeeId: "2", employeeName: "Akshay Poosan", employeeAge: "26",visible:false },
-    { employeeId: "3", employeeName: "Hanan Binth Basheer", employeeAge: "24",visible:false },
-    { employeeId: "4", employeeName: "Aysha Parveen", employeeAge: "23",visible:false },
-    { employeeId: "5", employeeName: "Sreeram", employeeAge: "25",visible:false },
-    { employeeId: "6", employeeName: "Darshan", employeeAge: "23",visible:false },
-    { employeeId: "7", employeeName: "Afsal Rahman", employeeAge: "25",visible:false },
-    { employeeId: "8", employeeName: "Fawaz MK", employeeAge: "22",visible:false },
-    { employeeId: "9", employeeName: "Sharron", employeeAge: "22" ,visible:false},
-    { employeeId: "10", employeeName: "Razal", employeeAge: "23",visible:false },
+    { employeeId: "1", employeeName: "Athila Rahim", employeeAge: "23" ,visible:""},
+    { employeeId: "2", employeeName: "Akshay Poosan", employeeAge: "26",visible:"" },
+    { employeeId: "3", employeeName: "Hanan Binth Basheer", employeeAge: "24",visible:"" },
+    { employeeId: "4", employeeName: "Aysha Parveen", employeeAge: "23",visible:"" },
+    { employeeId: "5", employeeName: "Sreeram", employeeAge: "25",visible:"" },
+    { employeeId: "6", employeeName: "Darshan", employeeAge: "23",visible:"" },
+    { employeeId: "7", employeeName: "Afsal Rahman", employeeAge: "25",visible:"" },
+    { employeeId: "8", employeeName: "Fawaz MK", employeeAge: "22",visible:"" },
+    { employeeId: "9", employeeName: "Sharron", employeeAge: "22" ,visible:""},
+    { employeeId: "10", employeeName: "Razal", employeeAge: "23",visible:"" },
 
   ]
 
-  changeEmp(val1:any,val2:any,val3:any,i:any){
+  changeEmpId(val1:any,i:any){
     this.employeeDatas[i].employeeId=val1;
-    this.employeeDatas[i].employeeName=val2;
-    this.employeeDatas[i].employeeAge=val3;
-    this.employeeDatas[i].visible=false  
+
+    this.employeeDatas[i].visible=''  
+  }
+  changeEmpName(val1:any,i:any){
+
+    this.employeeDatas[i].employeeName=val1;
+
+    this.employeeDatas[i].visible=''  
+  }
+  changeEmpAge(val1:any,i:any){
+
+    this.employeeDatas[i].employeeAge=val1;
+    this.employeeDatas[i].visible=''  
   }
 }
 
